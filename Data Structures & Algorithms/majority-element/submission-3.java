de2376ba0e2e59492c62cbd93bf79fd3d09a28b7
Sri Soundharya K R <srisoundharyaramamoorthy@gmail.com>
2026-06-03 00:50:28 +0530

    Bulk sync: 21 submissions

---
+++ Data Structures & Algorithms/majority-element/submission-3.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int majority=nums[0],votes=1;
+        for(int i=0;i<nums.length;i++){
+            if (votes==0){
+                votes++;
+                majority=nums[i];
+            }
+            else if (majority==nums[i]){
+                votes++;
+            }
+            else{
+                votes--;
+            }
+        }
+        return majority;
+    }
+}
